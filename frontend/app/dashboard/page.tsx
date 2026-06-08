7eb50ac147c7e044ae643ed4a811c0ab6eb3aaf0
Prashant Roy <prashantroy2628@gmail.com>
2026-06-08 19:42:01 +0530

    Added image preview upload

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Menu, Plus, Mic } from "lucide-react";
 export default function Dashboard()
 
  {const [prompt, setPrompt] = useState("");
+  
 const welcomeMessages = [
   "Your move, Prashant.",
   "What's on your mind today?",
@@ -27,18 +28,30 @@ const [selectedImage, setSelectedImage] = useState<File | null>(null);
 const [loading, setLoading] = useState(false);
 const [showProjects, setShowProjects] = useState(false);
 
-const [projects] = useState([
-  "Logo Design",
-  "Brand Kit",
-  "Banner Design",
-  "AI Chat",
-]);
+const [projects, setProjects] = useState<string[]>([]);
+
+useEffect(() => {
+  const savedProjects = localStorage.getItem("projects");
+
+  if (savedProjects) {
+    setProjects(JSON.parse(savedProjects));
+  }
+}, []);
+
+useEffect(() => {
+  localStorage.setItem(
+    "projects",
+    JSON.stringify(projects)
+  );
+}, [projects]);
+
 const themes = [
   "black",
   "blue",
   "orange",
   "gradient",
   "blackblue",
+  "navyblack",
 ];
 
 const [theme] = useState(
@@ -82,22 +95,75 @@ const [theme] = useState(
   <div className="absolute inset-0 bg-gradient-to-br from-violet-900 via-black to-blue-900"></div>
 )}
 
-{theme === "blackblue" && (
+{theme === "gradient" && (
+  <div className="absolute inset-0 bg-gradient-to-br from-violet-900 via-black to-blue-900"></div>
+)}
+
+    {theme === "gradient" && (
+  <div
+    className="
+    absolute
+    inset-0
+    bg-gradient-to-b
+    from-[#f8fafc]
+    via-[#e2eefb]
+    to-[#8ec5f5]
+    "
+  ></div>
+)}
+{theme === "gradient" && (
   <>
-    <div className="absolute inset-0 bg-black"></div>
+    <div
+      className="
+      absolute
+      inset-0
+      bg-gradient-to-b
+      from-[#f8fafc]
+      via-[#e2eefb]
+      to-[#8ec5f5]
+      "
+    ></div>
 
     <div
       className="
       absolute
+      bottom-0
       left-1/2
-      top-1/2
       -translate-x-1/2
-      -translate-y-1/2
-      w-[1000px]
-      h-[600px]
-      bg-blue-600/10
+      w-[1200px]
+      h-[400px]
+      bg-sky-400/30
+      blur-[180px]
       rounded-full
-      blur-[220px]
+      "
+    ></div>
+  </>
+)}
+
+{theme === "navyblack" && (
+  <>
+    <div
+      className="
+      absolute
+      inset-0
+      bg-gradient-to-b
+      from-black
+      via-[#020817]
+      to-[#0f347d]
+      "
+    ></div>
+
+    <div
+      className="
+      absolute
+      bottom-[-200px]
+      left-1/2
+      -translate-x-1/2
+      w-[1400px]
+      h-[700px]
+      bg-blue-600/25
+      rounded-full
+      blur-[280px]
       "
     ></div>
   </>
@@ -107,10 +173,21 @@ const [theme] = useState(
       {/* Header */}
       <div className="flex items-center justify-between px-6 py-5">
 
-        <h1 className="text-3xl font-black">
-          Acompany
-          <span className="text-violet-400">AI</span>
-        </h1>
+        <h1 className="text-3xl font-black text-white">
+  Acompany
+  <span
+    className="
+    bg-gradient-to-r
+    from-[#A78BFA]
+    via-[#8B7CF8]
+    to-[#60A5FA]
+    bg-clip-text
+    text-transparent
+    "
+  >
+    AI
+  </span>
+</h1>
 
       <button
 className="
@@ -163,9 +240,36 @@ duration-300
 animate-[slideIn_.3s_ease]
 "
   >
-    <h2 className="text-xl font-semibold mb-6">
-      Recent Projects
-    </h2>
+    <div className="mb-6">
+
+<button
+  onClick={() => {
+    setMessages([]);
+    setShowProjects(false);
+  }}
+  className="
+  w-full
+  mb-4
+  p-3
+  rounded-xl
+  bg-white
+  text-black
+  font-semibold
+  hover:scale-[1.02]
+  transition-all
+  "
+>
+  + New Chat
+</button>
+
+  <h2 className="text-xl font-semibold">
+    Recent Projects
+  </h2>
+
+  <p className="text-sm text-white/50 mt-1">
+    {projects.length} Chats
+  </p>
+</div>
 
     <div className="space-y-3">
       {projects.map((project, index) => (
@@ -342,6 +446,52 @@ animate-[slideIn_.3s_ease]
   </div>
 
 </label>
+{selectedImage && (
+  <div
+   className="
+mb-4
+flex
+items-center
+gap-3
+bg-white/5
+backdrop-blur-xl
+border
+border-white/10
+rounded-2xl
+p-3
+w-fit
+shadow-lg
+"
+  >
+    <img
+      src={URL.createObjectURL(selectedImage)}
+      alt="preview"
+      className="
+w-14
+h-14
+object-cover
+rounded-xl
+"
+    />
+<div className="flex flex-col">
+  <span className="text-sm text-white">
+    {selectedImage.name}
+  </span>
+
+  <span className="text-xs text-white/50">
+    Image Ready
+  </span>
+</div>
+
+    <button
+    
+      onClick={() => setSelectedImage(null)}
+      className="text-red-400 text-sm"
+    >
+      ✕
+    </button>
+  </div>
+)}
           <input
   value={prompt}
   onChange={(e) => setPrompt(e.target.value)}
@@ -358,7 +508,17 @@ animate-[slideIn_.3s_ease]
     if (!prompt.trim()) return;
 
     const userMessage = prompt;
+setProjects((prev) => {
+  if (prev.includes(userMessage)) return prev;
 
+ const title =
+  userMessage.length > 25
+    ? userMessage.slice(0, 25) + "..."
+    : userMessage;
+
+return [title, ...prev].slice(0, 10);
+
+});
     setMessages([
       ...messages,
       {
