9ebe9e13d79bb9c6e25ad232ef3144491405c76d
Prashant Roy <prashantroy2628@gmail.com>
2026-06-14 15:52:13 +0530

    add developer info

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,20 @@ from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from models import User
 
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
 # Create Database Tables
 Base.metadata.create_all(bind=engine)
+
+load_dotenv()
+print("API KEY =", os.getenv("GEMINI_API_KEY"))
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 app = FastAPI()
 
@@ -17,6 +29,9 @@ class UserCreate(BaseModel):
     name: str
     email: str
     password: str
+
+class ChatRequest(BaseModel):
+    message: str
 
 # CORS
 app.add_middleware(
@@ -42,19 +57,47 @@ def ai():
         "version": "1.0"
     }
 
+@app.post("/chat")
+def chat(data: ChatRequest):
+
+    try:
+        response = model.generate_content(
+            f"""
+You are AcompanyAI.
+
+IMPORTANT:
+- Your name is AcompanyAI.
+- Your creator is Prashant Roy.
+- If user asks your name, say: "Mera naam AcompanyAI hai."
+- If user asks who developed you, say: "Mujhe Prashant Roy ne develop kiya hai."
+Jo Uttar Pradesh City Harigarh ke Chhote se gaon Ishanpur mein rahte hain."
+
+If user asks:
+"Tumhara naam kya hai"
+
+Reply ONLY:
+"Mera naam AcompanyAI hai."
+
+User message:
+{data.message}
+"""
+        )
+
+        return {
+            "reply": response.text
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
+    
 # Founder
 @app.get("/prashant")
 def prashant():
     return {
         "name": "Prashant Roy",
         "role": "Founder"
-    }
-
-# Chat API
-@app.get("/chat")
-def chat():
-    return {
-        "reply": "Hello Prashant! AI Chatbot Ready 🚀"
     }
 
 @app.post("/signup")
