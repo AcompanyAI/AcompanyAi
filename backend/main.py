7a33e88c6476b460271443b42d64e1abc706166d
Prashant Roy <prashantroy2628@gmail.com>
2026-06-16 19:57:49 +0530

    Fixed backend connection and voice

--- backend/main.py
+++ backend/main.py
@@ -44,12 +44,11 @@ class ChatRequest(BaseModel):
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # Home
 @app.get("/")
 def home():
